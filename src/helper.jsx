26801788a81907e5d7ab74da1fdc7a0a3a1920c6
Adinubaeze <emmanuelezeadinuba@gmail.com>
2024-02-17 09:52:20 +0100

    Done with setting up boilerplate

--- src/helper.jsx
+++
@@ -1,9 +0,0 @@
-export const setDarkMode = () =>{
-    document?.querySelector('body')?.setAttribute('data-theme', 'dark');
-}
-  
-export const setLightMode = () =>{
-    document?.querySelector('body')?.setAttribute('data-theme', 'light');
-}
-
-
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App"
-import { Provider } from 'react-redux';
-import { Theme } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Theme>
         <App />
-     </Theme>
   </React.StrictMode>
 );
 
